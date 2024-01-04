16dfa8d11363536e6434cdc61ea133c7fca4213c
GiorgiMakh <115108762+GiorgiMakh@users.noreply.github.com>
2024-01-04 17:26:38 +0400

    error handler

--- app.js
+++ app.js
@@ -32,8 +32,25 @@ app.get('*', (req, res) => {
   res.status(404).send('404: Page Not Found');
 });  
 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
 
 // Author: GiorgiMakh
